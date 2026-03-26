9b2ce12077f35d3ede8c9d0ffdb5cc3ff8db8cf0
shagirul hassan <146074274+RWshagirulhassan@users.noreply.github.com>
2026-03-26 15:10:29 +0530

    added config

--- src/main/java/com/readywealth/trading/trade_engine/marketdata/api/http/InstrumentSearchController.java
+++ src/main/java/com/readywealth/trading/trade_engine/marketdata/api/http/InstrumentSearchController.java
@@ -3,7 +3,6 @@ package com.readywealth.trading.trade_engine.marketdata.api.http;
 import com.readywealth.trading.trade_engine.marketdata.application.InstrumentSearchService;
 import com.readywealth.trading.trade_engine.marketdata.domain.InstrumentSearchResult;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/instruments")
-@CrossOrigin(origins = "http://localhost:5173")
 public class InstrumentSearchController {
 
     private final InstrumentSearchService service;
